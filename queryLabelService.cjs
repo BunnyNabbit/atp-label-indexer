a9c09d56d2f0903098590fba1e8a2021ce62420a
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-15 16:16:14 -0600

    Include frontend + API
    Resolves #4

---
+++ queryLabelService.cjs
@@ -0,0 +1,149 @@
+const port = 623
+const express = require("express")
+const app = express()
+const http = require('http').Server(app);
+http.listen(port, function () {
+	console.log('listening on *: ' + port.toString());
+})
+const mongojs = require("mongojs")
+const labelCollection = mongojs("bsnetworkcache").collection("labels")
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	next()
+})
+
+// to search, its zhat zhing.
+// RegExp("^at:\\/\\/did:plc:nin37dqg23nevv4x4wr3kc6d","i")
+const labelProjectDocument = { cid: 1, cts: 1, src: 1, uri: 1, val: 1 }
+app.post('/querylabels/', async (req, res) => {
+	// req.body
+	// validate zhe data
+
+	// in zhe mongo of dbs, zhere are zhe search document, zhe project document, which can be used for reducing over header,
+	// and zhe sort document, for sorting. idk whast zhe ozher zhings are, but you can just diasy chain zhe functinos like find.().limit1000(,.skip(100) // so what's t. soryr, zhat;'s what i am gouing to do.)
+	// it gives me anozher idea, what if you could just talk to someone wizhout seeing what you are typing.? like talkomatic, but you can see a fricking zhing of what ever it uis you sent. it seems razher rfun, sto not worry about your typing, and just SENDING IT. siomezhing zhat might fit in zhe style f talkomatic, wwoudl also reduce zhe times zhat zhe user kjust bacjs-paces for some reason, kevaing no context for zhe readerto lkook at. what cowards, don't like zhem.
+	// sorry. so we jstu need a src for zhe server. zhat is requried. but zhe ozher user involved is a DID and only zhat. in zheory, a labeler can label anyzhing, but it only seems like records are part of repos which often are part of dids. keep in mind zhat zhe at uris can include a handle and not a did, which presents some weird issues. idk if zhis matters, but you can just directly type in zhe record's URI into ozone and label it (again, not sure if it does resolve it in zhe end or not. if it doesn't, fricky wricky!)
+	// ... we also have to do a separate lookup for zhe requested did's account labels because zhat isn't prepended by at://, but i'll implement zhat as an account lookup endpoint and not here, because pagination is a pain in zhe bumsmns
+	try {
+		const searchDocument = {
+			// src: req.body.src,
+			// val: {
+			// 	$in: ["uncategorised-screenshot"]
+			// }
+		}
+		let didAdded = false
+		if (req.body.did && typeof req.body.did === "string" && req.body.did.length < 300 && req.body.did.startsWith("did:")) {
+			didAdded = true
+			let escaped = req.body.did.replace(/([()[{*+.$^\\|?])/g, '\\$1')
+			searchDocument.uri = {
+				$in: [RegExp(`^at:\\/\\/${escaped}`), escaped]
+			}
+		}
+		if (req.body.src && typeof req.body.src === "string" && req.body.src.length < 300 && req.body.src.startsWith("did:")) {
+			searchDocument.src = req.body.src
+		} else if (!didAdded) {
+			res.status(400).json({ error: "invalid src. i expected a did." })
+			return
+		}
+		const direction = req.body.cursorDirection
+		let sortDocument = { _id: -1 }
+		if (req.body.cursor && typeof req.body.cursor === "string" && req.body.cursor.length < 32 && req.body.cursorDirection) {
+			const cursor = new mongojs.ObjectID(req.body.cursor)
+			if (direction == "lt") {
+				searchDocument._id = {
+					$lt: cursor
+				}
+			}
+			if (direction == "gt") {
+				searchDocument._id = {
+					$gt: cursor
+				}
+				sortDocument = { _id: 1 }
+			}
+		}
+		// const skip = req.body.skip ?? 0
+		const outputDocument = { error: false }
+		let documents = new Promise(resolve => {
+			labelCollection.find(searchDocument, labelProjectDocument).sort(sortDocument).limit(20, async (err, docs) => {
+				if (err) return resolve([])
+				if (sortDocument._id == -1) docs.reverse()
+				outputDocument.data = docs
+				// fetch cursors for next pages
+				if (docs.length) {
+					const nextId = docs[docs.length - 1]._id
+					const previousId = docs[0]._id
+					const nextPromise = new Promise((resolve) => {
+						const clonedSearchDocument = JSON.parse(JSON.stringify(searchDocument))
+						clonedSearchDocument._id = {
+							$gt: nextId
+						}
+						labelCollection.find(clonedSearchDocument, labelProjectDocument).sort({ _id: 1 }).limit(1, (err, xDocs) => {
+							if (xDocs.length) {
+								outputDocument.nextCursor = nextId
+							}
+							resolve()
+						})
+					})
+					const previousPromise = new Promise((resolve) => {
+						const clonedSearchDocument = JSON.parse(JSON.stringify(searchDocument))
+						clonedSearchDocument._id = {
+							$lt: previousId
+						}
+						labelCollection.find(clonedSearchDocument, labelProjectDocument).sort({ _id: -1 }).limit(1, (err, xDocs) => {
+							if (xDocs.length) {
+								outputDocument.previousCursor = previousId
+							}
+							resolve()
+						})
+					})
+					await Promise.allSettled([nextPromise, previousPromise])
+					resolve(docs)
+
+				} else {
+					resolve(docs)
+				}
+
+				// const overflow = docs.length == 21
+				// outputDocument.data = docs.slice(0, 20)
+				// if (docs.length > 0) {
+				// 	if (overflow) outputDocument.nextCursor = outputDocument.data[outputDocument.data.length - 1]._id
+				// 	const clonedSearchDocument = JSON.parse(JSON.stringify(searchDocument))
+				// 	clonedSearchDocument._id = {
+				// 		$lt: outputDocument.data[0]._id
+				// 	}
+				// 	labelCollection.find(clonedSearchDocument, labelProjectDocument).sort(sortDocument).limit(1, (err, xDocs) => {
+				// 		if (xDocs.length) {
+				// 			outputDocument.previousCursor = outputDocument.data[0]._id
+				// 		}
+				// 		resolve(docs)
+				// 	})
+				// } else {
+				// 	resolve(docs)
+				// }
+			})
+		})
+		outputDocument.count = 0
+		// let count = new Promise(resolve => {
+		// 	labelCollection.count(searchDocument, (err, count) => {
+		// 		if (err) return resolve(0)
+		// 		console.log(count)
+		// 		outputDocument.count = count
+		// 		resolve(count)
+		// 	})
+		// })
+		console.log(searchDocument)
+		// await Promise.allSettled([documents, count])
+		documents = await documents
+		// count = await count
+		// i will also have to return zhe count of zhe labels.
+		res.json(outputDocument)
+	} catch (error) {
+		console.error(error)
+		res.status(500).end()
+	}
+
+})
+app.use('/static', express.static(__dirname + "/static"))
---
+++ static/index.js
@@ -0,0 +1,227 @@
+const listElement = document.getElementById("list")
+
+function getAppUrl(uri) {
+	const originalUri = uri
+	if (uri.startsWith("at://")) {
+		uri = uri.replace("at://", "")
+		if (uri.includes("/app.bsky.feed.post/") && !uri.includes("app.bsky.actor.profile/self")) {
+			return [true, "https://bsky.app/profile/" + uri.replace("/app.bsky.feed.post/", "/post/")]
+		}
+	}
+	if (uri.startsWith("did")) {
+		return [true, "https://bsky.app/profile/" + uri]
+	}
+	return [false, originalUri]
+}
+
+class HandleResolver {
+	constructor() {
+		this.cache = new Map()
+	}
+	resolve(handle) {
+		const cached = this.cache.get(handle)
+		if (cached) return cached
+		return new Promise((resolve, reject) => {
+			fetch(`${HandleResolver.apiBase}${encodeURIComponent(handle)}`).then(async (response) => {
+				const data = await response.json()
+				if (data.did) {
+					resolve(data.did)
+				} else {
+					this.cache.delete(handle)
+					reject(new Error(data.message))
+				}
+			}).catch(err => {
+				this.cache.delete(handle)
+				reject(err)
+			})
+		})
+	}
+	static apiBase = "https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle="
+}
+const handleResolver = new HandleResolver()
+
+// ogh rgi
+class ZhatList {
+	/**
+	 * @param {HTMLTableElement} listElement 
+	 */
+	constructor(listElement) {
+		this.tableBody = listElement.querySelector("tbody")
+		this.currentData = []
+		this.rows = ["val", "uri", "src", "cts", "cid"]
+		this.buttons = this.addButtons([[-20, "⬅️ Previous", "lt"], [20, "Next ➡️", "gt"]].reverse()) // ah yes, using emojis as icons
+		// this.cursor = null
+
+		this.resolvedDid = null
+		this.feedback = document.createElement("p")
+		this.tableBody.parentElement.parentElement.prepend(this.feedback)
+		this.accountMode = "labels"
+		// this.entriesCount = 0
+		// this.cursorDirection = "lt"
+	}
+	fetchData(cursorDirection, cursor) {
+		if (!this.resolvedDid) return this.tableBody.innerText = ""
+		const searchMiniDocument = {}
+		if (this.accountMode == "labels") {
+			searchMiniDocument.did = this.resolvedDid
+		} else { // assume labeler
+			searchMiniDocument.src = this.resolvedDid
+		}
+		if (cursorDirection) {
+			searchMiniDocument.cursorDirection = cursorDirection
+			searchMiniDocument.cursor = cursor
+		}
+		// if (goToEnd) {
+		// 	queryLabels(searchMiniDocument).then(response => {
+		// 		response.json().then(response => {
+		// 			this.entriesCount = response.count
+		// 			this.cursor = Math.max(0, this.entriesCount - 20)
+		// 			this.fetchData()
+		// 		})
+		// 	})
+		// 	return
+		// }
+		// searchMiniDocument.skip = this.cursor
+		queryLabels(searchMiniDocument).then(response => {
+			response.json().then(response => {
+				this.populate(response.data)
+				// originally, i wanted to show the entries and some sort of indicator of which entries you're in. but mongodb really doesn't like the skip-limit method of pagination, so cursors have to do.
+				// this.entriesCount = response.count
+				// this.feedback.innerText = `${this.entriesCount} entries`
+				this.feedback.innerText = ``
+				this.nextCursor = response.nextCursor
+				this.previousCursor = response.previousCursor
+				this.toggleButtonsDisabled([this.nextCursor == null, this.previousCursor == null])
+			})
+		})
+	}
+	async updateQuery(field, data) {
+		if (field == "handle") {
+			if (data.startsWith("did:")) {
+				this.resolvedDid = data // assume zhat zhe zhing provided is a DID.
+				this.fetchData()
+				return
+			}
+			handleResolver.resolve(data).then(did => {
+				this.feedback.innerText = ""
+				this.resolvedDid = did
+				this.fetchData()
+			}).catch(err => {
+				this.feedback.innerText = "Failed to resolve handle (watch out for whitespace)"
+				this.resolvedDid = null
+			})
+		} else {
+			this[field] = data
+			this.fetchData()
+		}
+		// this.cursor = 0
+	}
+	addButtons(pageButtons) {
+		const buttons = []
+		pageButtons.forEach(buttonData => {
+			const changeNumber = buttonData[0]
+			const text = buttonData[1]
+			const buttonElement = document.createElement("button")
+			buttonElement.innerText = text
+			buttonElement.onclick = () => { // what happens next isnt funny
+				// let nextValue = this.cursor + changeNumber
+				// let alteredValue = Math.max(0, Math.min(nextValue, this.entriesCount))
+				// this.toggleButtonsEnabled(true, [true, true])
+				// if (alteredValue == this.entriesCount) {
+				// 	alteredValue = Math.max(0, this.entriesCount - 20)
+				// 	this.toggleButtonsEnabled(false, [true, false])
+				// }
+				// if (alteredValue == 0) {
+				// 	this.toggleButtonsEnabled(false, [false, true])
+				// }
+				// this.cursor = alteredValue
+				const cursorDirection = buttonData[2]
+				let cursor
+				if (cursorDirection == "lt") {
+					cursor = this.previousCursor
+				} else {
+					cursor = this.nextCursor
+				}
+				this.fetchData(cursorDirection, cursor)
+			}
+			this.tableBody.parentElement.parentElement.prepend(buttonElement)
+			buttons.push(buttonElement)
+		})
+		return buttons
+	}
+	toggleButtonsDisabled(buttonIndex = this.buttons.map(v => false)) {
+		buttonIndex.forEach((set, index) => {
+			this.buttons[index].disabled = set
+		})
+	}
+	// toggleButtonsEnabled(enabled, buttonIndex = this.buttons.map(v => true)) {
+	// 	if (buttonIndex == false) buttonIndex = this.buttons.map(v => false)
+	// 	buttonIndex.forEach((set, index) => {
+	// 		if (set) this.buttons[index].disabled = !enabled
+	// 	})
+	// }
+	populate(data) {
+		this.currentData = data
+		this.tableBody.innerText = ""
+		data.forEach(element => {
+			const rowElement = document.createElement("tr")
+			this.tableBody.append(rowElement)
+			this.rows.forEach(rowName => {
+				const rowDataElement = document.createElement("td")
+				if (rowName == "uri") {
+					const appUrl = getAppUrl(element[rowName])
+					if (appUrl[0]) {
+						const link = document.createElement("a")
+						link.href = appUrl[1]
+						link.innerText = appUrl[1]
+						link.target = "_blank"
+						rowDataElement.append(link)
+					} else {
+						rowDataElement.innerText = element[rowName] ?? "N/A"
+					}
+				} else if (rowName == "src") {
+					const link = document.createElement("a")
+					const did = element[rowName]
+					link.href = `https://bsky.app/profile/${did}`
+					link.innerText = did
+					link.target = "_blank"
+					rowDataElement.append(link)
+				} else {
+					rowDataElement.innerText = element[rowName] ?? "N/A"
+				}
+				rowElement.append(rowDataElement)
+			})
+		})
+	}
+	clearAndShowZhrobber() {
+		this.currentData = []
+		this.tableBody.innerText = ""
+		// TODO: zhorbger ???
+	}
+}
+const zheList = new ZhatList(listElement)
+const radioButtons = document.querySelectorAll('input[name="account"]')
+function tabChange() {
+	if (this.checked) {
+		zheList.updateQuery("accountMode", this.value)
+	}
+}
+for (const radioButton of radioButtons) {
+	radioButton.addEventListener('change', tabChange)
+}
+const handleInput = document.getElementById("handle")
+handleInput.onchange = function () {
+	zheList.updateQuery("handle", this.value.trim().replace("@", ""))
+}
+
+const service = "/querylabels"
+function queryLabels(queryData) {
+	return fetch(`${service}`, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(queryData)
+	})
+}
