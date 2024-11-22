4ae7b51431f01ed00809ecb35c0a976260fb9f0a
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-22 13:33:54 -0600

    Label group counts
    Resolves #3

--- Database.cjs
+++ Database.cjs
@@ -5,19 +5,26 @@ class Database {
 		this.labelCollection = db.collection('labels')
 		this.cursorCollection = db.collection("labelCursors")
 		this.handleDidCollection = db.collection("resolveDid")
+		this.labelGroupCounts = db.collection("labelGroupCounts")
 	}
 
 	replaceLabel(label) {
 		return new Promise((resolve, reject) => {
-			this.labelCollection.replaceOne({ src: label.src, uri: label.uri, val: label.val }, label, { upsert: true }, (err) => {
-				resolve(true)
+			this.labelCollection.replaceOne({ src: label.src, uri: label.uri, val: label.val }, label, { upsert: true }, (err, status) => {
+				let upserted = false
+				if (status.upserted) upserted = status.upserted
+				this.incrementLabelGroupCount(label.src, label.val, upserted ? 1 : 0)
+				resolve(upserted)
 			})
 		})
 	}
 	deleteLabel(did, uri, val) {
 		return new Promise((resolve, reject) => {
-			this.labelCollection.remove({ src: did, uri, val }, (err) => {
-				resolve()
+			this.labelCollection.remove({ src: did, uri, val }, (err, status) => {
+				resolve(status.deletedCount)
+				if (status.deletedCount > 0) {
+					this.incrementLabelGroupCount(did, val, -1)
+				}
 			})
 		})
 	}
@@ -42,6 +49,52 @@ class Database {
 	getLabels(serviceDid) {
 		return new Promise((resolve, reject) => {
 			this.labelCollection.find({ src: serviceDid }, (err, docs) => {
+				resolve(docs)
+			})
+		})
+	}
+
+	incrementLabelGroupCount(src, val, increment) {
+		return new Promise((resolve, reject) => {
+			this.labelGroupCounts.updateOne(
+				{ _id: { src, val } },
+				{ $inc: { count: increment } },
+				{ upsert: true },
+				(err, result) => {
+					if (err) reject(err)
+					resolve(result)
+				}
+			)
+		})
+	}
+
+	generateLabelGroupCounts() {
+		return new Promise((resolve, reject) => {
+			this.labelCollection.aggregate([
+				{
+					$group: {
+						_id: { src: "$src", val: "$val" },
+						count: { $sum: 1 }
+					}
+				},
+				{
+					$out: "labelGroupCounts" // Output to the labelGroupCounts collection
+				}
+			], (err, result) => {
+				if (err) reject(err)
+				resolve(result)
+			})
+		})
+	}
+
+	getLabelGroupCounts(src, val) {
+		const query = {}
+		if (src) query["id.src"] = src
+		if (val) query["id.val"] = val
+
+		return new Promise((resolve, reject) => {
+			this.labelGroupCounts.find(query, (err, docs) => {
+				if (err) reject(err)
 				resolve(docs)
 			})
 		})
